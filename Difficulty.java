32a9d59f292dc08ff8f02d10fe98583a7beadaff
Patrik <patrik.lasso.ml@gmail.com>
2023-06-19 17:04:00 +0200

    Added Difficulty.java

---
+++ Difficulty.java
@@ -0,0 +1,28 @@
+public enum Difficulty {
+    BEGINNER(10, 10, 10),
+    INTERMEDIATE(16, 16, 40),
+    EXPERT(30, 16, 99);
+
+    private final int width;
+    private final int height;
+    private final int mines;
+
+
+    private Difficulty(int width, int height, int mines) {
+        this.width = width;
+        this.height = height;
+        this.mines = mines;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getMines() {
+        return mines;
+    }
+}
